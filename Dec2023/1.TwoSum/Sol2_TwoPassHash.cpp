f7713f93c4c79dee0cca0ae325dc904f108b0552
hrzhao76 <hrzhao76@gmail.com>
2023-12-05 22:45:25 +0100

    1. Two Sum

---
+++ Dec2023/1.TwoSum/Sol2_TwoPassHash.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        for(int i=0; i < nums.size(); i++){
+            numMap[nums[i]] = i;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement) && numMap[complement] != i) {
+                return {i, numMap[complement]};
+            }
+        }
+        
+        return {}; 
+    }
+};
